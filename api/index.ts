8ef9111d66db4d92e0d9e7446709d9237cbc3817
Myat Phone Paing <myatphonepaingpussi223@gmail.com>
2023-04-06 16:38:33 -0700

    commit three

---
+++ api/index.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+const app = express();
+app.use(express.static("public"));
+dotenv.config();
+const port = 4000;
+const users = { name: "mo mo", email: "momo@gmail.com", age: 20 };
+const apiurl = process.env.Url;
+console.log(apiurl);
+app.get("/users", (req: Request, res: Response) => {
+  res.send(users);
+});
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <script type ="text/javascript">
+    localStorage.setItem('apiurl','${apiurl}')
+    window.location.href = "/"
+    </script>
+  </body>
+</html>
+
+`;
+
+app.get("/api/url", (req: Request, res: Response) => {
+  res.send(html);
+});
+app.listen(port, () => {
+  console.log("I love ma ma");
+});
---
+++ public/script.js
@@ -0,0 +1,13 @@
+// const url = "http://localhost:4000/users";
+const fetchData = async () => {
+  const url = localStorage.getItem("apiurl");
+  if (url) {
+    const response = await fetch(`${url}/users`);
+    const data = await response.json();
+    console.log(data);
+  } else {
+    window.location.href = "/url";
+  }
+};
+
+fetchData();
