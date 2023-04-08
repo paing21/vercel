da6dff0c7acb9290f119fcb3c61f4805ba8acce8
Myat Phone Paing <myatphonepaingpussi223@gmail.com>
2023-04-08 16:24:28 -0700

    commit twel

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 const app = express();
-app.use(express.static("public"));
 dotenv.config();
 const port = 4000;
 const apiurl = process.env.Url;
-console.log(apiurl);
 
 const html = `
 <!DOCTYPE html>
@@ -25,13 +23,13 @@ const html = `
 </html>
 
 `;
-
+app.use(express.static("public"));
 app.get("/api", (req: Request, res: Response) => {
   res.send(html);
 });
 
 app.get("/api/users", (req: Request, res: Response) => {
-  res.send(JSON.stringify({ name: "mo mo", email: "momo@gmail.com", age: 20 }));
+  res.send(JSON.stringify({ name: "mo mo", age: 20 }));
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -2,11 +2,10 @@
 const fetchData = async () => {
   const url = localStorage.getItem("Url");
   if (url) {
-    console.log("I am single");
-    const response = await fetch(`${url}/users`);
-    // const response = await fetch(`${url}/users`);
+    const response = await fetch(url + "/users");
     const data = await response.json();
     console.log(data);
+    console.log("I am single");
   } else {
     window.location.href = "/api";
   }
