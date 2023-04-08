96d5fc12121bbc178a77e9d9ab5043fe66a89f09
Myat Phone Paing <myatphonepaingpussi223@gmail.com>
2023-04-08 15:36:56 -0700

    commit

--- public/script.js
+++ public/script.js
@@ -2,9 +2,10 @@
 const fetchData = async () => {
   const url = localStorage.getItem("Url");
   if (url) {
-    const response = await fetch(`${url}/users`);
-    const data = await response.json();
-    console.log(data);
+    console.log("I am single");
+    // const response = await fetch(`${url}/users`);
+    // const data = await response.json();
+    // console.log(data);
   } else {
     window.location.href = "/api";
   }
